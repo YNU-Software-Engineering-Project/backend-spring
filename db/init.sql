69964b2a29dc07aa7d284e7c671589cd43c43cf6
Jo Eungyeong <joeungyeong23@gmail.com>
2024-08-19 10:02:06 +0900

    update users table

--- db/init.sql
+++ db/init.sql
@@ -4,8 +4,9 @@ USE testdb;
 
 CREATE TABLE IF NOT EXISTS users (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	username VARCHAR(255) NOT NULL,
 	email VARCHAR(255) NOT NULL,
+    nickname VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
 
-INSERT INTO users (username, email) VALUES ('testuser', 'test@testmail.com');
+INSERT INTO users (email, nickname, password) VALUES ('test@testmail.com', 'testuser', 'password');
