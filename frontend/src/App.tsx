c32cb3fc242bdf26ea04b61359c667f982f60a96
kyjlab <kkl0110@naver.com>
2024-08-19 18:26:34 +0900

    Add SignUpTest component and related tests

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
+
 import DefaultPage from 'pages/DefaultPage';
 import TestPage from 'pages/TestPage';
+import SignUpTest from 'pages/SignUpTest';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<DefaultPage />} />
+      <Route path="/register" element ={<SignUpTest />} />
       <Route path="/test" element={<TestPage />} />
     </Routes>
   );
