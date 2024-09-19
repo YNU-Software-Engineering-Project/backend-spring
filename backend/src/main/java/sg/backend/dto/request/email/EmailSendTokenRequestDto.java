9891c15eeed95101ed88fa294899458dcc74e45c
Jo Eungyeong <joeungyeong23@gmail.com>
2024-09-20 01:20:54 +0900

    update validation message

--- backend/src/main/java/sg/backend/dto/request/email/EmailSendTokenRequestDto.java
+++ backend/src/main/java/sg/backend/dto/request/email/EmailSendTokenRequestDto.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class EmailSendTokenRequestDto {
 
-    @NotBlank(message = "이메일 주소를 입력해주세요.")
+    @NotBlank(message = "이메일 주소는 필수 입력값입니다.")
     @Email
     private String email;
 }
