952a16b9aab1f16301bc8dfb6ab685c3fc19a6cc
Jo Eungyeong <joeungyeong23@gmail.com>
2024-09-20 01:19:45 +0900

    add validation message

--- backend/src/main/java/sg/backend/dto/request/user/PatchUserProfileRequestDto.java
+++ backend/src/main/java/sg/backend/dto/request/user/PatchUserProfileRequestDto.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class PatchUserProfileRequestDto {
 
-    @NotBlank
+    @NotBlank(message = "닉네임은 필수 입력값입니다.")
     private String nickname;
 
     private String password;
