42138271ec04c2ea62df83a31957c8a8f75ff3f9
MinjiSeo16 <mjlove6630@naver.com>
2024-09-20 23:42:04 +0900

    add swagger auth

--- backend/src/main/java/sg/backend/config/SecurityConfig.java
+++ backend/src/main/java/sg/backend/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
