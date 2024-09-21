ed31c051672f5deb9b3eff856ca948a045ec87c3
MinjiSeo16 <mjlove6630@naver.com>
2024-09-21 15:11:16 +0900

    delete refresh token

--- backend/src/main/java/sg/backend/dto/response/CreateAccessTokenResponseDto.java
+++
@@ -1,9 +0,0 @@
-package sg.backend.dto.response;
-
-public class CreateAccessTokenResponseDto {
-    private String accessToken;
-
-    public CreateAccessTokenResponseDto(String accessToken) {
-        this.accessToken = accessToken;
-    }
-}
