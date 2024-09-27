33c4a718ac967ecea2bd751e4c0c8d4c99bbf708
MinjiSeo16 <mjlove6630@naver.com>
2024-09-27 17:46:40 +0900

    add swagger

--- backend/src/main/java/sg/backend/controller/AdminSummaryController.java
+++ backend/src/main/java/sg/backend/controller/AdminSummaryController.java
@@ -1,11 +1,18 @@
 package sg.backend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sg.backend.dto.response.ResponseDto;
 import sg.backend.dto.response.funding.AdminSummaryDto;
+import sg.backend.dto.response.funding.GetFundingByStateResponseDto;
 import sg.backend.service.AdminSummaryService;
 
 @RestController
@@ -15,6 +22,13 @@ public class AdminSummaryController {
 
     private final AdminSummaryService adminSummaryService;
 
+    @Operation(
+            summary = "관리자 요약 페이지 불러오기"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요약 요소들 불러오기 성공",
+                    content = @Content(schema = @Schema(implementation = AdminSummaryDto.class)))
+    })
     @GetMapping("/summary")
     public ResponseEntity<AdminSummaryDto> getAdminSummary(){
         AdminSummaryDto summary = adminSummaryService.getAdminSummary();
--- backend/src/main/java/sg/backend/controller/FundingBoardController.java
+++ backend/src/main/java/sg/backend/controller/FundingBoardController.java
@@ -1,11 +1,17 @@
 package sg.backend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sg.backend.dto.response.funding.AdminSummaryDto;
 import sg.backend.dto.response.funding.FundingDashboardResponseDto;
 import sg.backend.service.FundingBoardService;
 
@@ -16,6 +22,13 @@ public class FundingBoardController {
 
     private final FundingBoardService fundingBoardService;
 
+    @Operation(
+            summary = "펀딩 상황판 불러오기"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "상황판 불러오기 성공",
+                    content = @Content(schema = @Schema(implementation = FundingDashboardResponseDto.class)))
+    })
     @GetMapping("/{fundingId}/dashboard")
     public ResponseEntity<FundingDashboardResponseDto> getFundingDashboard(@PathVariable Long fundingId){
         FundingDashboardResponseDto dashboard = fundingBoardService.getFundingDashboard(fundingId);
