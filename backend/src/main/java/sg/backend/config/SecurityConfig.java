0c2ac5b8c54b33e44a1cf2425917811151857771
MinjiSeo16 <mjlove6630@naver.com>
2024-09-29 14:51:09 +0900

    edit securityConfig

--- backend/src/main/java/sg/backend/config/SecurityConfig.java
+++ backend/src/main/java/sg/backend/config/SecurityConfig.java
@@ -40,8 +40,7 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()
-                        .requestMatchers("/api/**", "/file/**").permitAll()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/**", "/file/**","/admin/**").permitAll()
                         .anyRequest().authenticated()
                 )
 
