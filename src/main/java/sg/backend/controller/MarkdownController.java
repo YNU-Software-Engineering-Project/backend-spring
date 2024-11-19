d95421c312931941ded631a45aaa6d9c4053c5a4
kes0226 <es7837@naver.com>
2024-11-20 01:18:40 +0900

    swagger

--- src/main/java/sg/backend/controller/MarkdownController.java
+++ src/main/java/sg/backend/controller/MarkdownController.java
@@ -1,5 +1,10 @@
 package sg.backend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -11,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import sg.backend.dto.request.wirtefunding.StoryContentDto;
 import sg.backend.dto.response.ResponseDto;
+import sg.backend.dto.response.writefunding.GetFundingMainResponseDto;
 import sg.backend.dto.response.writefunding.project.StoryImageResponseDto;
 import sg.backend.service.PostService;
 
@@ -27,6 +33,13 @@ public class MarkdownController {
     private final PostService postService;
 
 
+    @Operation(summary = "toast ui 이미지 저장하기")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "서버에 이미지 저장 성공",
+                    content = @Content(schema = @Schema(implementation = GetFundingMainResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "이미지 저장 실패",
+                    content = @Content(schema = @Schema(implementation = ResponseDto.class)))
+    })
     @PostMapping("/{funding_id}/editor/image-upload")    //story_id를 넘겨줌
     @ResponseBody  // JSON 형식의 응답을 반환
     public ResponseEntity<? super StoryImageResponseDto> uploadEditorImage(
@@ -35,7 +48,13 @@ public class MarkdownController {
         return postService.file_upload(funding_id, image);
     }
 
-
+    @Operation(summary = "이미지 url")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "이미지 보여주기 성공",
+                    content = @Content(schema = @Schema(implementation = GetFundingMainResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "이미지 보여주기 실패",
+                    content = @Content(schema = @Schema(implementation = ResponseDto.class)))
+    })
     @GetMapping(value = "/editor/image-print/{filename}", produces = {
             MediaType.IMAGE_GIF_VALUE,
             MediaType.IMAGE_JPEG_VALUE,
@@ -73,7 +92,13 @@ public class MarkdownController {
     }
 
 
-    // 게시글 저장
+    @Operation(summary = "toast ui 저장")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "스토리 내용 저장 성고",
+                    content = @Content(schema = @Schema(implementation = GetFundingMainResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "스토리 내용 저장 실패",
+                    content = @Content(schema = @Schema(implementation = ResponseDto.class)))
+    })
     @PostMapping("/{funding_id}/editor/save")
     public ResponseEntity<ResponseDto> savePost(@RequestBody @Valid StoryContentDto content,
                                                 @PathVariable("funding_id") Long funding_id) {
--- src/main/java/sg/backend/controller/PaymentController.java
+++ src/main/java/sg/backend/controller/PaymentController.java
@@ -1,11 +1,18 @@
 package sg.backend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import sg.backend.dto.request.wirtefunding.PaySuccessRequestDto;
+import sg.backend.dto.response.ResponseDto;
+import sg.backend.dto.response.writefunding.GetFundingMainResponseDto;
 import sg.backend.dto.response.writefunding.PaySuccessResponseDto;
 import sg.backend.service.PaymentService;
 
@@ -16,6 +23,13 @@ public class PaymentController {
 
     private final PaymentService paymentService;
 
+    @Operation(summary = "결제 성공시 처리")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "결제 성공(결제 정보도 잘 들어감)",
+                    content = @Content(schema = @Schema(implementation = GetFundingMainResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "결제 실패",
+                    content = @Content(schema = @Schema(implementation = ResponseDto.class)))
+    })
     @PostMapping("/{funding_id}/pay/success")
     public ResponseEntity<? super PaySuccessResponseDto> paySuccess(
             @AuthenticationPrincipal(expression = "username") String email,
